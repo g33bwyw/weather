6768c229f6622bc7bcd6f2460e5c420171c812af
g33bwyw <g33bwyw@users.noreply.github.com>
2020-08-30 13:57:29 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidException.php
+++ src/Exceptions/InvalidException.php
@@ -7,9 +7,9 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
+
 namespace Wangyw\Weather\Exceptions;
 
 class InvalidException extends \Exception
 {
-
 }
